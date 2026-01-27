c5755f89d5014380fc2a37b55feb5b9a31893eea
Vijayakumar-Harish <vijayakumarharisha@gmail.com>
2026-01-27 20:49:17 +0530

    chore: add api versioning

--- backend/src/infrastructure/server/createServer.ts
+++ backend/src/infrastructure/server/createServer.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import { userRoutes } from "../../app/modules/user/user.routes";
 import { authRoutes } from "../../app/modules/auth/auth.routes";
+import { skillRoutes } from "../../app/modules/skill/skill.routes";
 export function createServer() {
   const app = Fastify({
     logger: true,
@@ -19,5 +20,9 @@ export function createServer() {
   });
   app.register(authRoutes, { prefix: "/api" });
   app.register(userRoutes, { prefix: "/api" });
+  app.register(authRoutes, { prefix: "/api/v1" });
+  app.register(userRoutes, { prefix: "/api/v1" });
+  app.register(skillRoutes, { prefix: "/api/v1" });
+
   return app;
 }
